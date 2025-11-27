56cdf0faf49fb467bda03fbeb3fcc8c1713a3bb3
PR3S1G4 <153407926+PR3S1G4zZ@users.noreply.github.com>
2025-11-27 09:54:23 -0500

    Fix namespace declaration and initialize Name property

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,9 +1,10 @@
-namespace ConsumoAPI2.Api.Models;
-
-public class Product
+namespace ConsumoAPI2.Api.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = default!;
-    public decimal Price { get; set; }
-    public int Stock { get; set; }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
 }
